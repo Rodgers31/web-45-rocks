6813fca27222fc36e3ce3e213c641877a1c89231
Rodgers31 <otienor986@gmail.com>
2021-09-02 12:58:08 -0400

    deployable

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+server.use(express.json());
+server.use(cors());
+
+server.get('/api', (req, res) => {
+	res.send(`<h1>We 45 is awesome!</>`);
+});
+server.get('/', (req, res) => {
+	res.send(`<h1>We 45 rocks</>`);
+});
+server.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
