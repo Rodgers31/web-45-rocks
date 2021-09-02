6baabb01bf8d87b0795365d8014395a482188706
Rodgers31 <otienor986@gmail.com>
2021-09-02 14:08:33 -0400

    Added react

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config(); // this allows to stash artificial en variables in a file
+const path = require('path');
+
 const express = require('express');
 const cors = require('cors');
 const server = express();
@@ -6,12 +8,17 @@ const server = express();
 const PORT = process.env.PORT || 5000;
 server.use(express.json());
 server.use(cors());
+server.use(express.static(path.join(__dirname, 'client/learning/build')));
 
 server.get('/api', (req, res) => {
 	res.send(`<h1>We 45 is awesome!</>`);
 });
 server.get('/', (req, res) => {
 	res.send(`<h1>We 45 rocks</>`);
+});
+
+server.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/learning/build', 'index.html'));
 });
 server.listen(PORT, () => {
 	console.log(`listening on port ${PORT}`);
